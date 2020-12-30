d1fac4adde925e52965a44a8e8c1168dda7df82c
Therealaveragejoe <58762590+Therealaveragejoe@users.noreply.github.com>
2020-12-29 23:56:38 -0600

    create deadPeople render and color change for dead people in work

--- dead-neighbors/src/DeadPeople.js
+++ dead-neighbors/src/DeadPeople.js
@@ -5,18 +5,36 @@ class DeadPeople extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            people:''
+            deadPersons:[],
+            aliveSVGFillStroke:['#e3d6b1','#9f6255'],
+            deadSVGFillStroke:['#FF0000','#9f6255'],
+            deadSinceVisit:20
         }
     }
-    render(){
-        return(
-            <div style ={{display:'flex',flexDirection:'row', flexWrap:'wrap'}}>
-            <DeadPerson/>
-            </div>
-        );
- 
+    componentDidMount = ()=>{
+        let people = [];
+        for(let i = 1; i<=100; i++){
+            people.push(
+                <div key = {i} className= {`person${i}`}>
+                <DeadPerson 
+                    key = {i}
+                    className = {`deadPerson${i}`}
+                    fillStroke={i - this.state.deadSinceVisit > 0 ? this.state.aliveSVGFillStroke : this.state.deadSVGFillStroke}
+                />
+                </div>
+            )
+        }
+        this.setState({deadPersons: people})
     }
+    render(){
+    return(
+        <div style ={{display:'flex',flexDirection:'row', flexWrap:'wrap'}}>
+            {this.state.deadPersons.map((people)=>{
+                console.log(people)
+                return(people);
+            })}
+        </div>
+    )}
 }
-
 
 export default DeadPeople;
--- dead-neighbors/src/DeadPerson.js
+++ dead-neighbors/src/DeadPerson.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const DeadPerson=()=>{
+const DeadPerson=({fillStroke})=>{
+    console.log(fillStroke)
     return(
     <div style={{width:'60px', height:'70px'}}>
         <svg viewBox = '0 10 100 100'>
             <rect x='35' y='70' rx='10' ry='10' width='30' height='50' 
-                style={{fill:'#e3d6b1',stroke:'#9f6255',strokewidth:'5',fillrule:'evenodd'}} />
+                style={{fill:fillStroke[0],stroke:fillStroke[1],strokewidth:'5',fillrule:'evenodd'}} />
             <circle cx = '50'  cy = '50' r='25'
-                style={{fill:'#e3d6b1',stroke:'#9f6255',strokewidth:'5',fillrule:'evenodd'}} />
+                style={{fill:fillStroke[0],stroke:fillStroke[1],strokewidth:'5',fillrule:'evenodd'}} />
         </svg>
     </div>
     )
