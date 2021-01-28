0bad398cb22ab8aac87ebc4296c59e538aa8da2a
Therealaveragejoe <58762590+Therealaveragejoe@users.noreply.github.com>
2021-01-28 13:40:40 -0600

    write about statement, style about/blog page

--- dead-neighbors/src/About.js
+++ dead-neighbors/src/About.js
@@ -2,20 +2,22 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className = "about">
-      <div className="content" style={{flexDirection:"column"}}>
-        <h2>About--Placeholder</h2>
+    <div className="about">
+      <div className="content" style={{ flexDirection: "column" }}>
+        <h3>About Dead Neighbors</h3>
         <p>
-          Ut incididunt pariatur dolore labore commodo sunt enim aute nulla
-          officia amet ullamco aute culpa.
+          The mission of Dead Neighbors is to help show you that the numbers you
+          see on the screen actually represent your children, mothers, fathers,
+          grandparents, friends and neighbors.
         </p>
         <p>
-          Ut incididunt pariatur dolore labore commodo sunt enim aute nulla
-          officia amet ullamco aute culpa.
+          In todays day and age, it is so easy to move on to the next thing very
+          quickly. There is so much information out in the world and it
+          distracts us from the fact that people are sick, suffering, and dying.
         </p>
         <p>
-          Ut incididunt pariatur dolore labore commodo sunt enim aute nulla
-          officia amet ullamco aute culpa.
+          Please take a moment to let the numbers soak in and feel the struggle
+          and loss in your neighbors hearts.
         </p>
       </div>
     </div>
