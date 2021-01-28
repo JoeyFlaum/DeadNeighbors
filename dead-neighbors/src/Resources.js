1c7270c0a60e91bba43c0edeedea9d4824597221
Therealaveragejoe <58762590+Therealaveragejoe@users.noreply.github.com>
2021-01-28 14:26:43 -0600

    add cdc link and style

--- dead-neighbors/src/Resources.js
+++ dead-neighbors/src/Resources.js
@@ -2,25 +2,25 @@ import React from "react";
 
 const Resources = () => {
   return (
-    <div className = 'resources'>
-      <div className="infoCard">
-        Resources--Placeholder
+    <div className="resources">
+      <div className="infoCard cdc">
+        <a
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+          href="https://www.cdc.gov/coronavirus/2019-ncov/index.html?s_cid=bb-coronavirus-2019-ncov-NCIRD"
+          title="image of SARS-CoV-2"
+        >
+          <img
+            src="http://www.cdc.gov/coronavirus/2019-ncov/images/Coronavirus-badge-300.png"
+            alt="Coronavirus Disease 2019 (COVID-19)"
+          />
+        </a>
       </div>
-      <div className="infoCard">
-        Resources--Placeholder
-      </div>
-      <div className="infoCard">
-        Resources--Placeholder
-      </div>
-      <div className="infoCard">
-        Resources--Placeholder
-      </div>
-      <div className="infoCard" >
-        Resources--Placeholder
-      </div>
-      <div className="infoCard">
-        Resources--Placeholder
-      </div>
+      <div className="infoCard">Resources--Placeholder</div>
+      <div className="infoCard">Resources--Placeholder</div>
+      <div className="infoCard">Resources--Placeholder</div>
+      <div className="infoCard">Resources--Placeholder</div>
+      <div className="infoCard">Resources--Placeholder</div>
     </div>
   );
 };
