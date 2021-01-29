1f7d05c84af36851377e67eb43f28def96eef376
Therealaveragejoe <58762590+Therealaveragejoe@users.noreply.github.com>
2021-01-28 23:24:09 -0600

    Style and photos for resource page. Remove hidden scroll from info page.

--- dead-neighbors/src/Resources.js
+++ dead-neighbors/src/Resources.js
@@ -1,26 +1,44 @@
 import React from "react";
+import WHO from "./Images/WHO.jpg";
+import NIH from "./Images/NIH.jpg";
 
 const Resources = () => {
   return (
     <div className="resources">
       <div className="infoCard cdc">
-        <a
-          target="_blank"
-          rel="nofollow noopener noreferrer"
-          href="https://www.cdc.gov/coronavirus/2019-ncov/index.html?s_cid=bb-coronavirus-2019-ncov-NCIRD"
-          title="image of SARS-CoV-2"
-        >
           <img
             src="http://www.cdc.gov/coronavirus/2019-ncov/images/Coronavirus-badge-300.png"
             alt="Coronavirus Disease 2019 (COVID-19)"
           />
+        <a
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+          href="https://www.cdc.gov/coronavirus/2019-ncov/index.html?s_cid=bb-coronavirus-2019-ncov-NCIRD"
+        >
+        <button><div className = "click-me">Centers for Disease Control</div></button>
         </a>
       </div>
-      <div className="infoCard">Resources--Placeholder</div>
-      <div className="infoCard">Resources--Placeholder</div>
-      <div className="infoCard">Resources--Placeholder</div>
-      <div className="infoCard">Resources--Placeholder</div>
-      <div className="infoCard">Resources--Placeholder</div>
+      <div className="infoCard who">
+         <div className = 'copywright'>© WHO / Blink Media - Bart Verweij</div>
+          <img src={WHO} alt="World Health Organization Teaching Class Awareness" />
+        <a
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+          href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019"
+        >
+        <button><div className = "click-me">World Heath Organization</div></button>
+        </a>
+      </div>
+      <div className="infoCard nih">
+          <img src={NIH} alt="National Institues of Health Virus"/>
+        <a
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+          href="https://covid19.nih.gov/"
+        >
+        <button><div className = "click-me">National Institutes of Health</div></button>
+        </a>
+      </div>
     </div>
   );
 };
