f69aaa190707d71f997fc38eb008ab5d5bea28b8
Therealaveragejoe <58762590+Therealaveragejoe@users.noreply.github.com>
2021-02-15 16:37:23 -0600

    correct grammar on about page

--- dead-neighbors/src/AboutPage/About.js
+++ dead-neighbors/src/AboutPage/About.js
@@ -16,14 +16,14 @@ const About = ({ deadPerson }) => {
             fathers, grandparents, friends and neighbors.
           </p>
           <p>
-            In todays day and age, it is so easy to move on to the next thing
+            In today's day and age, it is so easy to move on to the next thing
             very quickly. There is so much information out in the world and it
             distracts us from the fact that people are sick, suffering, and
             dying.
           </p>
           <p>
             Please take a moment to let the numbers soak in and feel the
-            struggle and loss in your neighbors hearts.
+            struggle and loss in your neighbors' hearts.
           </p>
         </div>
       </div>
